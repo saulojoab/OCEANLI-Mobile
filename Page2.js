8f98a0422266f4d946c210286d5440c76e147534
seriesbd1517@gmail.com <seriesbd1517@gmail.com>
2019-10-20 10:43:31 -0300

    ranking screen

---
+++ Page2.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View style={styles.cont}>
+        <Image source={require('./img/rank.png')} style={styles.image}></Image>
+      </View>
+      
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  cont: {
+    flex: 1,
+    backgroundColor: '#436cce'
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+     
+    resizeMode: 'contain'
+}
+});
